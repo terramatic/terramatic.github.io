b8ecc287fe013e5a7c774334d0fc79ac095f004b
jsalcal <jsalcal@gmail.com>
2019-06-14 16:21:11 +0200

    image resize

--- scripts/country_thumbs.py
+++ scripts/country_thumbs.py
@@ -1,15 +1,34 @@
 import os
 from PIL import Image
 
-FULL_DIR = r'C:\xampp\htdocs\images\fulls\morroco'
-THUMBS_DIR = r'C:\xampp\htdocs\images\thumbs\morroco'
+SRC_DIR = r'C:\xampp\htdocs\travelmatic\images\src\aruba'
+FULL_DIR = r'C:\xampp\htdocs\travelmatic\images\fulls\aruba'
+THUMBS_DIR = r'C:\xampp\htdocs\travelmatic\images\thumbs\aruba'
 
 ## Create thumbnails
-size = [256, 256]
-for f in os.listdir(FULL_DIR):
-	infile = os.path.join(FULL_DIR, f)
-	outfile = os.path.join(THUMBS_DIR, f)
-	print('thumbnail {} to {}'.format(infile, outfile))
+xsW = 256
+xlW = 1920
+
+print SRC_DIR
+for f in os.listdir(SRC_DIR):
+	# Filenames
+	outfileXL = os.path.join(FULL_DIR, f)
+	outfileXS = os.path.join(THUMBS_DIR, f)
+	
+	# Source
+	infile = os.path.join(SRC_DIR, f)
 	im = Image.open(infile)
-	im.thumbnail(size)
-	im.save(outfile, "JPEG")
+	wpercent = (xsW/float(im.size[0]))
+	xsH = int((float(im.size[1])*float(wpercent)))
+	wpercent = (xlW/float(im.size[0]))
+	xlH = int((float(im.size[1])*float(wpercent)))
+
+	# XS
+	print('thumbnail {} to {}'.format(infile, outfileXS))
+	img = im.resize((xsW, xsH), Image.ANTIALIAS)
+	img.save(outfileXS, "JPEG")
+
+	# XL
+	print('thumbnail {} to {}'.format(infile, outfileXL))	
+	img = im.resize((xlW, xlH), Image.ANTIALIAS)
+	img.save(outfileXL, "JPEG")	
