d2142324d1128c2f2111e48bf9b414cde2d1e39c
jsalcal <jsalcal@gmail.com>
2019-06-21 23:07:55 +0200

    china and indonesia

--- scripts/country_copy.py
+++ scripts/country_copy.py
@@ -8,13 +8,13 @@ if len(sys.argv) != 2:
 	exit()
 country = sys.argv[1]
 
-ORI_DIR = r'D:\Photos\Norway 2016'
+ORI_DIR = r'D:\Photos\Indonesia 2017'
 SRC_DIR = r'C:\xampp\htdocs\travelmatic\images\src\{}'.format(country)
 if not os.path.exists(SRC_DIR):
     os.makedirs(SRC_DIR)
 
 ## Copy selected images from src to SRC_DIR
-sel_imgs = ['1250039', '1250088', '1250099', '240692', '240732', '240753', '240760', '240778', '240803', '240821', '240867', '240947', '240957', '240962', '1250036']
+sel_imgs = [ '0829', '70010', '70026', '70052', '70053', '70125', '70221', '70224', '70303', '70353', '70390', '70674', '70690', '70764', '70767', '8068', '80103', '80238', '80470', '80583'  ]
 for im in sel_imgs:
 	if im == '': continue
 	pattern = '{d}/*{a}*'.format(d=ORI_DIR, a=im)	
